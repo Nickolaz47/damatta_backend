9e2a5a11ef95443167cc57a2c37919689660d105
Nicolas da Matta <nicolasaraujo47@gmail.com>
2022-11-21 21:35:14 -0300

    Created saleController.

---
+++ controllers/saleController.ts
@@ -0,0 +1,75 @@
+// Types
+import { Request, Response } from "express";
+// Models
+import Sale from "../models/Sale";
+import User from "../models/User";
+
+const getSales = async (req: Request, res: Response) => {
+  const { user } = req;
+
+  const userExists = await User.findOne({ where: { id: user.id } });
+  if (!userExists) {
+    return res.status(404).json({ errors: ["Usuário não encontrado!"] });
+  }
+
+  const sales = await Sale.findAll({ where: { UserId: user.id } });
+
+  return res.json({ sales });
+};
+
+const createSale = async (req: Request, res: Response) => {
+  const { user } = req;
+  const { seller, buyer, value, commission, agent } = req.body;
+
+  const userExists = await User.findOne({ where: { id: user.id } });
+  if (!userExists) {
+    return res.status(404).json({ errors: ["Usuário não encontrado!"] });
+  }
+
+  const sale = { seller, buyer, value, commission, agent, UserId: user.id };
+  const newSale = await Sale.create(sale);
+
+  return res.status(201).json({ sale: newSale });
+};
+
+const updateSale = async (req: Request, res: Response) => {
+  const { user } = req;
+  const { id } = req.params;
+  const { seller, buyer, value, commission, agent } = req.body;
+
+  const userExists = await User.findOne({ where: { id: user.id } });
+  if (!userExists) {
+    return res.status(404).json({ errors: ["Usuário não encontrado!"] });
+  }
+
+  const saleToUpdate = await Sale.findOne({ where: { id, UserId: user.id } });
+  if (!saleToUpdate) {
+    return res.status(404).json({ errors: ["Venda não encontrada!"] });
+  }
+
+  await saleToUpdate.update({ seller, buyer, value, commission, agent });
+
+  return res.json({ sale: saleToUpdate });
+};
+
+const deleteSale = async (req: Request, res: Response) => {
+  const { user } = req;
+  const { id } = req.params;
+
+  const userExists = await User.findOne({ where: { id: user.id } });
+  if (!userExists) {
+    return res.status(404).json({ errors: ["Usuário não encontrado!"] });
+  }
+
+  const saleToDelete = await Sale.findOne({ where: { id, UserId: user.id } });
+  if (!saleToDelete) {
+    return res.status(404).json({ errors: ["Venda não encontrada!"] });
+  }
+
+  await saleToDelete.destroy();
+
+  return res.json({ sale: saleToDelete });
+};
+
+const saleController = { getSales, createSale, updateSale, deleteSale };
+export default saleController;
